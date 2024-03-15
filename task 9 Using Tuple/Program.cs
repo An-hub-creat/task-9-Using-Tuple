1d1b41e26f327a9ca8c40b7c53dd12b3b68c5bc9
An-hub-creat <ann.ghazaryn2004@gmail.com>
2024-03-15 11:30:04 +0400

    Add project files.

---
+++ task 9 Using Tuple/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = 1;
+        double b = -3;
+        double c = 2;
+
+        var solutions = SolveQuadratic(a, b, c);
+
+        Console.WriteLine("x1 = " + solutions.Item1);
+        Console.WriteLine("x2 = " + solutions.Item2);
+    }
+
+    static Tuple<double, double> SolveQuadratic(double a, double b, double c)
+    {
+        double discriminant = b * b - 4 * a * c;
+        double x1, x2;
+
+        if (discriminant >= 0)
+        {
+            x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+        }
+        else
+        {
+            // Complex roots, set x1 and x2 to NaN
+            x1 = double.NaN;
+            x2 = double.NaN;
+        }
+
+        return Tuple.Create(x1, x2);
+    }
+}
